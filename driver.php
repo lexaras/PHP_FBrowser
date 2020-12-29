e32882050b5cbd89152117d5ae5dca72fc80165e
DESKTOP-EEBU2QN\Ben <benaskan@gmail.com>
2020-12-30 00:22:01 +0200

    Updates

---
+++ driver.php
@@ -0,0 +1,94 @@
+<?php $url = $_SERVER['REQUEST_URI'];
+print("<h1>Directory contents: $url</h1>");
+//Logout button 
+print('<div class="logout">Click here to <a href="index.php?action=logout"> logout.</div>');
+?>
+<table>
+    <thead>
+        <tr>
+            <th>Type</th>
+            <th>Name</th>
+            <th>Actions</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        //Display cwd(current working directory) files and directories
+        if (!isset($_GET['path']) && !isset($_POST['new_dir']) && empty($_POST)) {
+            $dir = getcwd();
+            // print_r($dir);
+            $files_directories = scandir($dir);
+            // print('<br>');
+            // print_r($files_directories);
+            display($files_directories);
+        };
+        //Navigate between folders if path has changed and display cwd again
+        if (isset($_GET) && $_GET['path'] != "") {
+            $current_dir = $_GET['path'];
+            // print($current_dir);
+            chdir($current_dir);
+            $dir = getcwd();
+            //  print($dir);
+            $files_directories = scandir($dir);
+            navigate($files_directories, $url);
+        }
+        //Create new directory
+        if (isset($_POST['new_dir']) && $_POST['new_dir'] != "") {
+            $dir = getcwd();
+            //print($dir);
+            mkdir($dir . '/' . $_POST['new_dir']);
+            header('Location: ' . $_SERVER['REQUEST_URI']);
+            $files_directories = scandir($dir);
+            display($files_directories);
+        };
+        //Delete file
+        if (!empty($_POST) && !isset($_POST['new_dir'])) {
+            $file_name = str_replace("_", ".", array_keys($_POST)[0]);
+            // print($file_name);
+            unlink($file_name);
+            header('Location: ' . $_SERVER['REQUEST_URI']);
+            $dir = getcwd();
+            $files_directories = scandir($dir);
+            display($files_directories);
+        }
+        ?>
+    </tbody>
+</table>
+<?php
+//Back button
+$current_dir = $_SERVER['REQUEST_URI'];
+$previous_dir = dirname($current_dir);
+print("<button><a href='$previous_dir'>Back</a></button>");
+//history.back(1)- not working properly when making new dir
+//print('<button type="button" onclick="history.back(1);">Back</button>'); 
+//New directory input + button fields
+print("<form action=''method='POST'><input type='text' name='new_dir' 
+id='input' placeholder='Name of new directory'><button id='submit'>Submit</button></form>");
+
+?>
+
+
+
+<!-- Functions -- !>
+<?php
+//Looping through array of files/directories and displaying each 
+//into the table with a single if condition(whether its file or dir)
+function display($files_directories)
+{
+    foreach ($files_directories as $each) {
+        if ($each != '.' && $each != '..')
+            if (is_dir($each)) {
+                print("<tr><td>Folder</td><td><a href='?path=$each'>" . $each . "</a></td><td></td></tr>");
+            } else print("<tr><td>File</td><td>" . $each . "</a></td><td><form method='POST'><input type='submit' name='$each' value='Delete'></form></td></tr>");
+    };
+};
+function navigate($files_directories, $url)
+{
+    foreach ($files_directories as $each) {
+        if ($each != '.' && $each != '..')
+            if (is_dir($each)) {
+                print("<tr><td>Folder</td><td><a href='$url/$each'>" . $each . "</a></td><td></td></tr>");
+            } else print("<tr><td>File</td><td>" . $each . "</a></td><td><form method='POST'><input type='submit' name='$each' value='Delete'></form></td></tr><br>");
+    };
+}
+?>
--- index.php
+++ index.php
@@ -1,56 +1,55 @@
-<?php 
-session_start();
-//logout logic
-if(isset($_GET['action']) && $_GET['action'] == 'logout'){
-    session_start();
-    unset($_SESSION['username']);
-    unset($_SESSION['password']);
-    unset($_SESSION['logged_in']);
-    print('Logged out!');
-}
-?>
-
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
     <title>File browser</title>
 </head>
+
 <body>
-    <h2>Enter Username and Password</h2>
+    <?php
+    session_start();
+    if (
+        isset($_POST['login'])
+        && !empty($_POST['username'])
+        && !empty($_POST['password'])
+    ) {
+        if (
+            $_POST['username'] == 'Benas' &&
+            $_POST['password'] == '12345'
+        ) {
+            $_SESSION['logged_in'] = true;
+            $_SESSION['timeout'] = time();
+            $_SESSION['username'] = "Benas";
+        } else {
+            print('<h4> Wrong username or password</h4>');
+        }
+    }
+    ?>
+
     <div>
         <?php
-        $msg = '';
-        if(isset($_POST['login'])
-            && !empty($_POST['username'])
-            && !empty($_POST['password'])
-        ){
-            if ($_POST['username'] == 'Benas' && 
-            $_POST['password']== 'qwerty123')
-            {
-                $_SESSION['logged_in'] = true;
-                $_SESSION['timeout'] = time();
-                $_SESSION['username'] = "Benas";
-                echo 'You have entered valid username and password';
-            } else{
-                $msg = 'Wrong username or password';
-            }
+        //logout logic
+        if (isset($_GET['action']) && $_GET['action'] == 'logout') {
+            session_start();
+            unset($_SESSION['username']);
+            unset($_SESSION['password']);
+            unset($_SESSION['logged_in']);
+            print('<h4>You have been successfully logged out!</h4>');
         }
+        //login logic
+        if ($_SESSION['logged_in'] == true) {
+            require('driver.php');
+        } else {
         ?>
-    </div>
-    <div>
-        <?php
-        if($_SESSION['logged_in']==true){
-          //  require(kitas page);
-        }
-        ?>
-        <form action="./mano.php" method="post">
-        <h4><?php echo $msg; ?></h4>
-        <input type="text" name="username" placeholder="username = Benas" required autofocus></br>
-        <input type="password" name="password" placeholder="password = qwerty123" required>
-        <button class="btn" type="submit" name="login">Login</button>
-    </form>
-    Click here to<a href="index.php?action=logout"> logout.
+            <form id="login" action="./index.php" method="post">
+                <input type="text" name="username" placeholder="username = Benas" required autofocus></br>
+                <input type="password" name="password" placeholder="password = 12345" required>
+                <button class="button1" type="submit" name="login">Login</button>
+            <?php } ?>
     </div>
 </body>
+
 </html>
